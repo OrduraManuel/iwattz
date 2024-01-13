10608a676960f43f26ade51094f580f047498c10
m.ordura <m.ordura@octavianlab.com>
2024-01-13 12:18:26 +0100

    blurring behind ctaContainer desktop without teleport

--- src/components/modal/btnModal.vue
+++ src/components/modal/btnModal.vue
@@ -6,17 +6,28 @@ let isWhere = '#'+props.where
 console.log(isWhere,'questo è il div')
 </script>
 <template>
-  <Teleport to="Body">
-    <div class="ctaContainer">
-        <a  class="createBtn" data-bs-toggle="modal" :data-bs-target="isWhere">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            Contattami
-        </a>
-    </div>
+  <Teleport to="body"> <!-- mobile teleport -->
+      <div class="ctaContainer d-md-none">
+          <a  class="createBtn" data-bs-toggle="modal" :data-bs-target="isWhere">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              Contattami
+          </a>
+      </div>
   </Teleport>
+
+  <div class="ctaContainer d-none d-md-block"><!-- desktop not teleport -->
+    <a  class="createBtn" data-bs-toggle="modal" :data-bs-target="isWhere">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Contattami
+    </a>
+  </div>
+
 </template>
 <style lang="scss" scoped>
 
